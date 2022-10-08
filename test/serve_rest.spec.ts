b7612e9021b690e1e86ff020edfbee2e82b63eeb
Daniel Milak <mlkdaniel99@gmail.com>
2022-10-08 11:41:20 -0300

    Fim_aula

--- test/serve_rest.spec.ts
+++ test/serve_rest.spec.ts
@@ -2,6 +2,7 @@ import pactum from 'pactum';
 import { SimpleReporter } from '../simple-reporter';
 
 describe('ServeRest API', () => {
+  let carrinho = '';
   let token = '';
   const p = pactum;
   const rep = SimpleReporter;
@@ -16,10 +17,11 @@ describe('ServeRest API', () => {
         .spec()
         .post(`${baseUrl}/login`)
         .withJson({
-          email: 'a@b.com.br',
-          password: 'admin.1234'
+          email: 'fulano@qa.com',
+          password: 'teste'
         })
         .expectStatus(200)
+        .inspect()
         .returns('authorization');
     });
   });
@@ -30,6 +32,32 @@ describe('ServeRest API', () => {
         .spec()
         .get(`${baseUrl}/usuarios`)
         .withHeaders('Authorization', token)
+        .expectStatus(200);
+    });
+  });
+
+  describe('Cadastro', () => {
+    it('POST', async () => {
+      carrinho = await p
+        .spec()
+        .post(`${baseUrl}/carrinhos`)
+        .withHeaders('Authorization', token)
+        .withJson({
+          idProduto: '123456789',
+          quantidade: 1
+        })
+        .returns('_id')
+        .inspect()
+        .expectStatus(200);
+    });
+  });
+
+  describe('carrinhos', () => {
+    it('GET ALL', async () => {
+      await p
+        .spec()
+        .get(`${baseUrl}/carrinhos/` + carrinho)
+        .withHeaders('Authorization', carrinho)
         .expectStatus(200);
     });
   });
